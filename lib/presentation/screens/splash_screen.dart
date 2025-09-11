d8db877186f83dc4e88179ebbbd408fa0d4aec1e
muina <jordanmuina@gmail.com>
2025-09-10 17:29:15 -0700

    ajoute de la page splascreen

--- lib/presentation/screens/splash_screen.dart
+++ lib/presentation/screens/splash_screen.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color(0XFF129575),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/image 11.png'),
+            const SizedBox(height: 10),
+            Text('T E M U', style: TextStyle(
+              fontFamily: 'Poppins',
+              //fontWeight: FontWeight.bold,
+              fontSize: 30,
+              color: Colors.white
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
