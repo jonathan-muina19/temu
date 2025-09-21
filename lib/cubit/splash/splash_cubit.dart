b46dc3bf092c3b21f28400f40f4e6747c99d844d
muina <jordanmuina@gmail.com>
2025-09-21 15:42:08 -0700

    on implemente le cubit pour le splashscreen et le shared preferences pour ne plus afficher le onboarding si l'utilisateur a deja ete en contact avec l'appli

---
+++ lib/cubit/splash/splash_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:temu/cubit/splash/splash_state.dart';
+
+class SplashCubit extends Cubit<SplashState>{
+  SplashCubit() : super(SplashState(status: SplashStatus.initial));// On demare le spash
+
+  // Cette methode est asynchrone car on va attendre le timer
+  // Elle sera appelee depuis le initial() du splashscreen
+  Future<void> checkFirstLaunch() async{
+    await Future.delayed(const Duration(seconds: 4)); // Timer du splash
+
+    // On cree une instance de SharedPreferences
+    final prefs = await SharedPreferences.getInstance();
+
+    final onboardingDone = prefs.getBool('onboarding_done') ?? false;
+
+    if(onboardingDone){
+      emit(SplashState(status: SplashStatus.notFirstLauch));
+    }else{
+      emit(SplashState(status: SplashStatus.firstLauch));
+    }
+  }
+}
